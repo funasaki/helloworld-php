8252b4c2627c11de33730a169925b08eaef49d43
EC2 Default User <ec2-user@ip-10-0-1-181.ap-northeast-1.compute.internal>
2015-07-06 06:03:01 +0000

    5th

---
+++ deploy_staging.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+export AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEY_ID}
+export AWS_SECRET_ACCESS_KEY=${AWS_SECRET_ACCESS_KEY}
+export AWS_DEFAULT_REGION="ap-northeast-1"
+
+aws ec2 describe-instances
+
